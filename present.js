7924d8f63bb586e50ba2bb7a8f5ff27785b961cc
Joe Baldwin <jcameronbaldwin@gmail.com>
2020-05-19 22:15:30 +0100

    New project code refactoring

--- present.js
+++ present.js
@@ -1,117 +1,120 @@
-const nextSlide = document.querySelector("a.next")
-const prevSlide = document.querySelector("a.previous")
-const sliderTag = document.querySelector("div.slider")
-const stepsTag = document.querySelector("span.steps")
-const footerTag = document.querySelector("footer")
-let currentSlide = 0
-let totalSlides = 0
+// Elements
+const nextSlide = document.querySelector("a.next");
+const prevSlide = document.querySelector("a.previous");
+const sliderDiv = document.querySelector("div.slider");
+const stepCounter = document.querySelector("span.steps");
+const footer = document.querySelector("footer");
+const loadingMessage = document.querySelector("header p.loading");
 
-const loadingTag = document.querySelector("header p.loading")// Loading message
-const filesApi = "https://api.figma.com/v1/files/"          // Files endpoint
-const imagesApi = "https://api.figma.com/v1/images/"        // Images endpoint
-const project = "Rc9MpQo5whSUaAJV8ifwW6KU"                  // Figma project ID
-const apiKey = "19110-d2b5a588-0210-468a-9c42-c2335bca6e21" // Figma API key
-const apiHeaders = {                                        // Headers for auth
+// API
+const filesApi = "https://api.figma.com/v1/files/";
+const imagesApi = "https://api.figma.com/v1/images/";
+const projectKey = "LaGLDWRHVLWMMryCCFcCfe"; // Figma Project ID
+const apiKey = "19110-d2b5a588-0210-468a-9c42-c2335bca6e21";
+const apiHeaders = {
   method: "GET",
   headers: {
-    "X-Figma-Token": apiKey
-  }
-}
+    "X-Figma-Token": apiKey,
+  },
+};
+
+let currentSlide = 0;
+let totalSlides = 0;
 
 // When page loads
 // 1. Load the file from figma & organise by IDs
 // 2. Then use the array of IDs to generate image URLs
 // 3. Then add images to the site
 
-const loadFile = function(key) {
-  return fetch(filesApi + key, apiHeaders)
-    .then(response => response.json())
-    .then(data => {
+const loadFile = (projectKey) => {
+  return fetch(filesApi + projectKey, apiHeaders)
+    .then((response) => response.json())
+    .then((data) => {
       // Get the frame IDs from the project
-      const ids = data.document.children[0].children.map(frame => {
-        return frame.id
-      })
+      const ids = data.document.children[2].children.map((frame) => {
+        return frame.id;
+      });
       // Get the frame names from the project
-      const names = data.document.children[0].children.map(frame => {
-        return frame.name
-      })
+      const names = data.document.children[2].children.map((frame) => {
+        return frame.name;
+      });
       // Return a new object
       return {
-        key: key,
+        key: projectKey,
         title: data.name,
         names: names,
-        ids: ids  // This value is an array of frame IDs returned by map
-      }
-    })
-}
+        ids: ids,
+      };
+    });
+};
 
-const loadImages = function(object) {
-  const key = object.key           // Project key
-  const ids = object.ids.join(",") // Seperate array items with comma for endpoint query
+const loadImages = (projectData) => {
+  const projectKey = projectData.key; // Project key
+  const ids = projectData.ids.join(","); // Separate array items with comma for endpoint query
 
-  return fetch(imagesApi + key + "?ids=" + ids + "&scale=1", apiHeaders)
-    .then(response => response.json())
-    .then(data => {
-      return object.ids.map(id => {
+  return fetch(imagesApi + projectKey + "?ids=" + ids + "&scale=1", apiHeaders)
+    .then((response) => response.json())
+    .then((data) => {
+      return projectData.ids.map((id) => {
         // Return image URLs for each frame based on the ID from loadFile
-        return data.images[id]
-      })
-    })
-}
+        return data.images[id];
+      });
+    });
+};
 
-const addImagesToSite = function(urls) {
-  sliderTag.innerHTML = ""
-  totalSlides = urls.length
-  stepsTag.innerHTML = `1 / ${totalSlides}`
-  footerTag.classList.add("show")
+const addImagesToSite = (urls) => {
+  sliderDiv.innerHTML = "";
+  totalSlides = urls.length;
+  stepCounter.innerHTML = `1 / ${totalSlides}`;
+  footer.classList.add("show");
 
   // Add urls from loadImages into an image tag to display on the screen
-  urls.forEach(url => {
-    sliderTag.innerHTML = sliderTag.innerHTML + `
+  urls.forEach((url) => {
+    sliderDiv.innerHTML =
+      sliderDiv.innerHTML +
+      `
       <div>
-        <img src="${url}">
+        <img src="${url}" alt="Client Preview">
       </div>
-    `
-  })
-}
+    `;
+  });
+};
 
-loadFile(project)                                // Load files from chosen project, return a new object
-  .then(file => {                                // Change loading message
-      loadingTag.innerHTML = file.title
-      document.title = file.title + " - Present"
-    return file                                  // Data is unchanged
+loadFile(projectKey)
+  .then((projectData) => {
+    loadingMessage.innerHTML = projectData.title;
+    document.title = `${projectData.title} - Client Presentation`;
+    return projectData; // Unchanged
   })
-  .then(file => loadImages(file))                // Handle image URLs from API data
-  .then(imageUrls => addImagesToSite(imageUrls)) // Add each item in image URL array to site
-
+  .then((projectData) => loadImages(projectData)) // Handle image URLs from API data
+  .then((imageUrls) => addImagesToSite(imageUrls)); // Add each item in image URL array to site
 
 // Slideshow events
-
-const next = function() {
-  currentSlide = currentSlide + 1
+const next = () => {
+  currentSlide = currentSlide + 1;
   if (currentSlide >= totalSlides) {
-    currentSlide = 0
+    currentSlide = 0;
   }
-  moveSlider()
-}
+  moveSlider();
+};
 
-const previous = function() {
-  currentSlide = currentSlide - 1
+const previous = () => {
+  currentSlide = currentSlide - 1;
   if (currentSlide < 0) {
-    currentSlide = totalSlides - 1
+    currentSlide = totalSlides - 1;
   }
-  moveSlider()
-}
+  moveSlider();
+};
 
-const moveSlider = function() {
-  sliderTag.style.transform = `translateX(${currentSlide * -100}vw)`
-  stepsTag.innerHTML = `${currentSlide + 1} / ${totalSlides}`
-}
+const moveSlider = () => {
+  sliderDiv.style.transform = `translateX(${currentSlide * -100}vw)`;
+  stepCounter.innerHTML = `${currentSlide + 1} / ${totalSlides}`;
+};
 
-nextSlide.addEventListener("click", function() {
-  next()
-})
+nextSlide.addEventListener("click", () => {
+  next();
+});
 
-prevSlide.addEventListener("click", function() {
-  previous()
-})
+prevSlide.addEventListener("click", () => {
+  previous();
+});
